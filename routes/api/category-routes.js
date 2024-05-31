cb70c6cc07d60fc05a6f2d54ab67368c7e1327fa
Andres Gomez <andregom2009@gmail.com>
2024-05-31 16:30:46 +0200

    category routes created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,61 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categories = await Category.findAll({include:[{model: Product}]});
+    res.status(200).json(categories);
+  }
+  catch(err){
+    res.status(500).json({message: 'Error retrieving categories'});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const category = await Category.findByPK(req.params.id, {include: [{model:Product}]});
+    res.status(200).json(category);
+  }
+  catch(err){
+    res.status(500).json({message: 'Error retrieving id'});
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new category
+  try{
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  }
+  catch(err){
+    res.status(500).json({message: 'Error creating new category'});
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try{
+    const updatedCategory = await Category.update(req.body, {where: {id: req.params.id}});
+    res.status(200).json(updatedCategory);
+  }
+  catch(err){
+    res.status(500).json({message: 'Error updating category'});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  try{
+    const deletedCat = await Category.destroy({where: {id: req.params.id}});
+    res.status(200).json(deletedCat);
+  }
+  catch(err){
+    res.status(500).json({message: 'Error deleting category'});
+  }
 });
 
 module.exports = router;
